845701aed198025b069431a1562c5d3d8d30f99b
Alex Chetverov <alexu-che@yandex.ru>
2020-03-29 18:37:21 +0300

    Create rim.py

---
+++ rim.py
@@ -0,0 +1,30 @@
+def decToRoman(num):
+    coding = zip(
+        [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+        ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    )
+    if num <= 0 or num >= 4000 or int(num) != num:
+        raise ValueError('Число должно быть не более 3999')
+    result = []
+    for d, r in coding:
+        while num >= d:
+            result.append(r)
+            num -= d
+    return ''.join(result)
+
+def RomanToDec(strng):
+    dec = 0
+    srtng = strng.upper()
+    if  not ("I" in strng or "X" in strng or "L" in strng or "C" in strng or "D" in strng or "M" in strng):
+        raise ValueError("Не верный формат римского числа")
+    coding = dict(M=1000, D=500, C=100, L=50, X=10,V=5, I=1)
+    for i in range(0, len(strng)-1):
+        if coding[strng[i]] < coding[strng[i+1]]:
+            dec -= coding[strng[i]]
+        else:
+            dec += coding[strng[i]]
+    dec += coding[strng[-1]]
+    return dec
+
+print(RomanToDec("MCMXCIX"))
+print(decToRoman(1999))
